36726192c071069ab50e8d6ea36fb9d9a114d38d
Tang Wei <wei.tang@coinflex.com>
2021-10-20 17:43:33 +0800

    solidity division rounding

--- contracts/OverflowV0_8.sol
+++ contracts/OverflowV0_8.sol
@@ -2,7 +2,14 @@ pragma solidity ^0.8.9;
 
 contract OverflowIntermediateValueV0_8 {
     function testOverflow() public pure returns (uint){
+
         uint max_uint = 2 ** 256 - 1;
         return max_uint * 2 / 3;
+    }
+
+    function testDivideRounding() public pure returns (bool){
+        
+        uint max_uint = 2 ** 256 - 1;
+        return max_uint / 1e18 * 1e18 == max_uint;
     }
 }
